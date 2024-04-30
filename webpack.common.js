106fdad2f7e45d40e24cafa260c9de514032c214
sam <abluecar@gmail.com>
2024-04-30 19:55:57 -0400

    Save initial files

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "(Change Me) CSS Grid Basic Layout",
+      filename: "index.html", // default value
+      template: "./src/template.html",
+      inject: "head",
+      scriptLoading: "defer",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: "single", // For multiple entries, checks that modules do not instantiate more than once
+  },
+};
